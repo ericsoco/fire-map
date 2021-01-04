bb746eabfe92e58a007d93016272c350859df1d4
Eric Socolofsky <eric@transmote.com>
2021-01-03 21:59:40 -0800

    Slider graph width (#11)
    * Move derived state requiring memoization out of selectors and into useMemo hooks. Still debugging tho.
    
    * Filter out fires with no name; correctly extract latest perimeters
    Fix slider bar chart spacing
    Add volta / engines
    Refine tick labels

--- src/state/fires-selectors.js
+++ src/state/fires-selectors.js
@@ -1,40 +1,13 @@
+export function selectAllFires() {
+  return state => state.fires.years;
+}
+
 export function selectAllFiresForYearRequest(year) {
   return state => state.fires.years[year]?.all || null;
 }
 
-export function selectCompleteFiresRequests() {
-  return state =>
-    Object.keys(state.fires.years).reduce(
-      (completeFires, year) => ({
-        ...completeFires,
-        [year]: state.fires.years[year].complete,
-      }),
-      {}
-    );
-}
-
 export function selectCompleteFiresForYearRequest(year) {
   return state => state.fires.years[year]?.complete || null;
-}
-
-/**
- * Retrieve all fires from the specified year back to the first empty year,
- * whether due to not-yet-loaded data or the beginning of the available data,
- * arranged in chronological order.
- */
-export function selectCompleteFiresBeforeYearRequest(year) {
-  return state => {
-    let nextYear = year - 1;
-    let request;
-    let years = [];
-    while (
-      (request = selectCompleteFiresForYearRequest(nextYear)(state)) !== null
-    ) {
-      years.unshift(request);
-      nextYear--;
-    }
-    return years;
-  };
 }
 
 /**
